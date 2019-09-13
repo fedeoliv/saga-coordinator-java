7c8706ec74b7213ec05f73c5f594103890c79e11
Fernando De Oliveira <fedeoliv@microsoft.com>
2019-09-13 17:01:24 -0300

    Maven with jlink initial structure

---
+++ v2/src/main/java/com/sample/coordinator/App.java
@@ -0,0 +1,12 @@
+package com.sample.coordinator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
---
+++ v2/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.sample.jlink {
+    requires org.slf4j;
+    exports com.sample.coordinator;
+    // export com.sample.app to com.specific.package;
+}
