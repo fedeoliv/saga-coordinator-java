adb28e4a105e00b06de9bacd0b0d15867b045038
Fernando De Oliveira <fedeoliv@microsoft.com>
2019-09-20 13:23:26 -0300

    Spring boot structure with moditect plugin to create runtime images

---
+++ v2/src/main/java/com/sample/coordinator/CoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.sample.coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinatorApplication.class, args);
+	}
+
+}
--- v2/src/main/java/module-info.java
+++ v2/src/main/java/module-info.java
@@ -1,5 +1,14 @@
-module com.sample.jlink {
-    requires org.slf4j;
+module com.sample.coordinator.jlink {
+    // requires spring.beans;
+    // requires spring.boot;
+    // requires spring.boot.autoconfigure;
+    // requires spring.context;
+    // requires spring.core;
+    // requires jdk.unsupported;
+
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    
     exports com.sample.coordinator;
     // export com.sample.app to com.specific.package;
 }
